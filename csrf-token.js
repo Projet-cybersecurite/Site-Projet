ffd3a61790c10733bccb0581b4dfc77e4bc12416
Aykisw <aymankemel04@gmail.com>
2025-03-12 13:07:54 +0100

    Create csrf-token.js

---
+++ csrf-token.js
@@ -0,0 +1,9 @@
+import crypto from 'crypto';
+
+export default async function handler(req, res) {
+    if (req.method !== 'GET') return res.status(405).json({ error: 'Méthode non autorisée' });
+
+    const csrfToken = crypto.randomBytes(20).toString('hex');
+    res.setHeader('Set-Cookie', `csrfToken=${csrfToken}; HttpOnly; Secure; SameSite=Strict; Path=/`);
+    res.status(200).json({ csrfToken });
+}
