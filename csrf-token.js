85c3342af1458bbccf08451da8f14d9bfdf5ccd2
Aykisw <aymankemel04@gmail.com>
2025-03-14 00:08:50 +0100

    Update csrf-token.js

--- csrf-token.js
+++ csrf-token.js
@@ -4,6 +4,6 @@ export default async function handler(req, res) {
     if (req.method !== 'GET') return res.status(405).json({ error: 'Méthode non autorisée' });
 
     const csrfToken = crypto.randomBytes(20).toString('hex');
-    res.setHeader('Set-Cookie', `csrfToken=${csrfToken}; HttpOnly; Secure; SameSite=Strict; Path=/`);
+    res.setHeader('Set-Cookie', `csrfToken=${csrfToken}; HttpOnly; Secure; SameSite=Lax; Path=/`);
     res.status(200).json({ csrfToken });
 }
