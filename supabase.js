a66281b04870503fac573ce490c49e9af09123e8
Aykisw <aymankemel04@gmail.com>
2025-03-13 23:23:37 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,12 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Vérification des variables d'environnement
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("⚠️ Erreur : Les variables d'environnement Supabase ne sont pas définies !");
 }
 
-// Connexion à Supabase
-export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
